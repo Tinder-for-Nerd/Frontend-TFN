00d426a7cfd69725ee3a863cdcb560d841df7a7d
vyas senthilkumar <vyas.sk17@gmail.com>
2026-05-05 08:55:49 +0530

    Fit discover card and hide bottom nav

--- src/modules/dashboard/pages/DiscoverPage.jsx
+++ src/modules/dashboard/pages/DiscoverPage.jsx
@@ -98,7 +98,7 @@ export function DiscoverPage({ variant }) {
           onMouseUp={handleMouseUp}
           onMouseLeave={handleMouseUp}
           className={`pm-discover__stack ${isDragging ? 'is-dragging' : ''}`}
-          style={{ height: `min(${CONTAINER_HEIGHT}px, calc(100dvh - 220px))` }}
+          style={{ height: `min(${CONTAINER_HEIGHT}px, calc(100dvh - 160px))` }}
         >
           <div
             className="pm-discover__rail"
